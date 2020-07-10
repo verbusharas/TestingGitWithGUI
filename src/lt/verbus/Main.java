a28accd4af369817b90c45732f074d509cbc340a
Sarunas Verbus <sarunas@verbus.lt>
2020-07-10 14:25:41 +0200

    init commit

---
+++ src/lt/verbus/Main.java
@@ -0,0 +1,8 @@
+package lt.verbus;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/lt/verbus/Test.java
@@ -0,0 +1,4 @@
+package lt.verbus;
+
+public class Test {
+}
